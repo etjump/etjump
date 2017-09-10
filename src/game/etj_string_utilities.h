ccc4e9636bade355f780ed9070297bc97409cfea
Jussi Haapanen <haapanen.jussi@gmail.com>
2017-09-10 22:40:56 +0300

    Added string utilities

---
+++ src/game/etj_string_utilities.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <boost/format.hpp>
+#include <string>
+#include <vector>
+
+namespace ETJump
+{
+	std::string hash(const std::string& input);
+	std::string newGuid();
+	std::string getBestMatch(const std::vector<std::string>& words, const std::string& current);
+	std::string sanitize(const std::string& text, bool toLower = false);
+	// returns the value if it's specified, else the default value
+	std::string getValue(const char *value, const std::string& defaultValue = "");
+	std::string getValue(const std::string& value, const std::string& defaultValue = "");
+
+	inline std::string stringFormat(boost::format& fmt)
+	{
+		return fmt.str();
+	}
+
+	template<typename T, typename... Targs>
+	std::string stringFormat(boost::format& fmt, const T& value, const Targs&... Fargs)
+	{
+		fmt % value;
+		return stringFormat(fmt, Fargs...);
+	}
+
+	template<typename T, typename... Targs>
+	std::string stringFormat(const std::string& fmt, const T& value, const Targs&... Fargs)
+	{
+		return stringFormat(boost::format(fmt) % value, Fargs...);
+	}
+}
