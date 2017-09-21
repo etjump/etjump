1c2bbe90c9bac1585e951f74e4e6b2640f0af36b
isRyven <ryven.mt@gmail.com>
2017-09-21 14:43:06 +0300

    added more bush and leaves shader

--- src/cgame/etj_draw_leaves_handler.h
+++ src/cgame/etj_draw_leaves_handler.h
@@ -19,8 +19,19 @@ namespace ETJump
 			"models/mapobjects/tree_temperate_sd/leaves_temperate2",
 			"models/mapobjects/tree_temperate_sd/leaves_temperate3",
 			"models/mapobjects/tree_desert_sd/palm_leaf1",
-			//"models/mapobjects/tree_desert_sd/palm_leaves",
 			"models/mapobjects/tree_desert_sd/palm_leaves2",
+			"models/mapobjects/plants_sd/bush_desert1",
+			"models/mapobjects/plants_sd/bush_desert2",
+			"models/mapobjects/plants_sd/bush_snow1",
+			"models/mapobjects/plants_sd/catail1",
+			"models/mapobjects/plants_sd/catail2",
+			"models/mapobjects/plants_sd/deadbranch1",
+			"models/mapobjects/plants_sd/deadbranch1_damp",
+			"models/mapobjects/plants_sd/deadbranch2",
+			"models/mapobjects/plants_sd/deadbranch3",
+			"models/mapobjects/plants_sd/grassfoliage1",
+			"models/mapobjects/plants_sd/grass_dry3",
+			"models/mapobjects/plants_sd/grass_green1",
 		};
 
 		void turnOnLeaves();
