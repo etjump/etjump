6360fd900032f53d2bbc1ef52cfeb1cd0d793a5a
isRyven <ryven.mt@gmail.com>
2018-05-15 14:20:44 +0300

    add indicator to aid quick follow
    Draws little text under the name to denote the ability to spectate
    the player, can be toggled off along with the quick follow feature
    using `etj_quickfollow` cvar.

---
+++ src/cgame/etj_quick_follow_drawable.cpp
@@ -0,0 +1,33 @@
+#include "etj_quick_follow_drawable.h"
+#include "../game/etj_string_utilities.h"
+
+char *BindingFromName(const char *cvar);
+
+ETJump::QuickFollowDrawer::QuickFollowDrawer() {}
+
+void ETJump::QuickFollowDrawer::beforeRender() {}
+
+void ETJump::QuickFollowDrawer::render() const
+{
+	if (canSkipDraw())
+	{
+		return;
+	}
+
+	float *color = CG_FadeColor(cg.crosshairClientTime, 1000);
+	if (!color)
+	{
+		return;
+	}
+
+	auto binding = BindingFromName("+activate");
+	auto hintText = stringFormat("^9[%s] to follow", binding);
+	float w = DrawStringWidth(cgs.clientinfo[cg.crosshairClientNum].name, 0.23f);
+	auto offx = SCREEN_CENTER_X - w / 2;
+	DrawString(offx, 182 + 10, 0.16f, 0.16f, color, qfalse, hintText.c_str(), 0, ITEM_TEXTSTYLE_SHADOWED);
+}
+
+bool ETJump::QuickFollowDrawer::canSkipDraw() const
+{
+	return etj_quickFollow.integer == 0 || cg.crosshairClientNum > MAX_CLIENTS;
+}
