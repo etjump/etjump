293c858abd595c1db6e92471a28ad7a90c0cce45
isRyven <ryven.mt@gmail.com>
2020-04-27 17:39:27 +0300

    move deps outside the src dir
    * remove unnecessary stuff

---
+++ src/game/bg_sha.cpp
@@ -0,0 +1,30 @@
+extern "C" {
+#include "sha1.h"
+}
+#ifdef CGAMEDLL
+#include "../cgame/cg_local.h"
+#else
+#include "../game/g_local.h"
+#endif
+
+const char *G_SHA1(const char *string)
+{
+	SHA1Context sha;
+
+	SHA1Reset(&sha);
+	SHA1Input(&sha, (const unsigned char *)string, strlen(string));
+
+	if (!SHA1Result(&sha))
+	{
+		return "";
+	}
+	else
+	{
+		return va("%08X%08X%08X%08X%08X",
+		          sha.Message_Digest[0],
+		          sha.Message_Digest[1],
+		          sha.Message_Digest[2],
+		          sha.Message_Digest[3],
+		          sha.Message_Digest[4]);
+	}
+}
