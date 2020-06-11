f9b2bc836913b36304bcd7f0e49209b03fad707d
isRyven <ryven.mt@gmail.com>
2020-06-11 15:00:36 +0300

    adjust gcc compilation flags
    * do little cmake cleanup
    * perform lto
    * strip symbols
    * strip dyntab symbols
    * run optimization flags over c++ translation units

--- src/build/pack.sh
+++ src/build/pack.sh
@@ -3,6 +3,5 @@ CWD_DIR="$PWD"
 cd ../../etjump
 zip -r "$CWD_DIR/unversioned_etjump.pk3" .
 cd "$CWD_DIR"
-zip unversioned_etjump.pk3 cgame.* ui.*
+zip unversioned_etjump.pk3 cgame.*.{so,dll} ui.*.{so,dll}
 echo "done"
-
