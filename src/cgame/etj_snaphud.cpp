c30ad4be3dfd07a3aaa38639736fc042e7ac327e
Aciz <arto.karkulahti@gmail.com>
2020-08-06 12:13:04 +0300

    Remove vars/funcs from header, make UpdateSnapHUDSettings static

--- src/cgame/etj_snaphud.cpp
+++ src/cgame/etj_snaphud.cpp
@@ -5,6 +5,44 @@
 
 namespace ETJump
 {
+	constexpr int SNAPHUD_MAXZONES{ 128 };
+	float snapSpeed;
+	float snapZones[SNAPHUD_MAXZONES];
+	int snapCount;
+
+	static int QDECL sortSnapZones(const void* a, const void* b)
+	{
+		return *(float*)a - *(float*)b;
+	}
+
+	// FIXME: share this with cgaz
+	static float PM_CalcScale(playerState_t* ps)
+	{
+		// based on PM_CmdScale from bg_pmove.c
+		float scale = ps->stats[STAT_USERCMD_BUTTONS] & (BUTTON_SPRINT << 8) && cg.pmext.sprintTime > 50 ? ps->sprintSpeedScale : ps->runSpeedScale;
+		return scale;
+	}
+
+	static void UpdateSnapHUDSettings(float speed)
+	{
+		float step;
+
+		snapSpeed = speed;
+		speed /= 125;
+		snapCount = 0;
+
+		for (step = floor(speed + 0.5) - 0.5; step > 0 && snapCount < SNAPHUD_MAXZONES - 2; step--)
+		{
+			snapZones[snapCount] = RAD2DEG(acos(step / speed));
+			snapCount++;
+			snapZones[snapCount] = RAD2DEG(asin(step / speed));
+			snapCount++;
+		}
+
+		qsort(snapZones, snapCount, sizeof(snapZones[0]), sortSnapZones);
+		snapZones[snapCount] = snapZones[0] + 90;
+	}
+
 	void DrawSnapHUD(void)
 	{
 		float scale;
@@ -68,38 +106,5 @@ namespace ETJump
 			CG_FillAngleYaw(snapZones[i] + 90, snapZones[i + 1] + 90, yaw, y, h, color[colorID]);
 			colorID ^= 1;
 		}
-	}
-
-	void UpdateSnapHUDSettings(float speed)
-	{
-		float step;
-
-		snapSpeed = speed;
-		speed /= 125;
-		snapCount = 0;
-
-		for (step = floor(speed + 0.5) - 0.5; step > 0 && snapCount < SNAPHUD_MAXZONES - 2; step--)
-		{
-			snapZones[snapCount] = RAD2DEG(acos(step / speed));
-			snapCount++;
-			snapZones[snapCount] = RAD2DEG(asin(step / speed));
-			snapCount++;
-		}
-
-		qsort(snapZones, snapCount, sizeof(snapZones[0]), sortSnapZones);
-		snapZones[snapCount] = snapZones[0] + 90;
-	}
-
-	// FIXME: share this with cgaz
-	static float PM_CalcScale(playerState_t* ps)
-	{
-		// based on PM_CmdScale from bg_pmove.c
-		float scale = ps->stats[STAT_USERCMD_BUTTONS] & (BUTTON_SPRINT << 8) && cg.pmext.sprintTime > 50 ? ps->sprintSpeedScale : ps->runSpeedScale;
-		return scale;
-	}
-
-	static int QDECL sortSnapZones(const void* a, const void* b)
-	{
-		return *(float*)a - *(float*)b;
 	}
 }
--- src/cgame/etj_snaphud.h
+++ src/cgame/etj_snaphud.h
@@ -4,13 +4,5 @@
 
 namespace ETJump
 {
-	constexpr int SNAPHUD_MAXZONES{ 128 };
-	float snapSpeed;
-	float snapZones[SNAPHUD_MAXZONES];
-	int snapCount;
-
 	void DrawSnapHUD();
-	void UpdateSnapHUDSettings(float speed);
-	static float PM_CalcScale(playerState_t* ps);
-	static int QDECL sortSnapZones(const void* a, const void* b);
 }
