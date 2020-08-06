5e277acb4b3ccc13e1e0dabfde3179e619ce7cdb
Aciz <arto.karkulahti@gmail.com>
2020-08-06 12:58:36 +0300

    Clean up cgaz header, move drawfuncs to cg_drawtools (#511)

--- src/cgame/cg_drawtools.cpp
+++ src/cgame/cg_drawtools.cpp
@@ -95,6 +95,62 @@ void CG_FillAngleYaw(float start, float end, float viewangle, float y, float hei
 	trap_R_SetColor(NULL);
 }
 
+/*
+==============
+PutPixel
+Used by CGaz 2
+==============
+*/
+// Dzikie
+void PutPixel(float x, float y)
+{
+	CG_DrawPic(x, y, 1, 1, cgs.media.whiteShader);
+}
+
+/*
+==============
+DrawLine
+Used by CGaz 2
+==============
+*/
+// Dzikie
+void DrawLine(float x1, float y1, float x2, float y2, vec4_t color)
+{
+	float len, stepx, stepy;
+	float i;
+
+	if (x1 == x2 && y1 == y2)
+	{
+		return;
+	}
+
+	trap_R_SetColor(color);
+
+	// Use a single DrawPic for horizontal or vertical lines
+	if (x1 == x2)
+	{
+		CG_DrawPic(x1, y1 < y2 ? y1 : y2, 1, abs(y1 - y2), cgs.media.whiteShader);
+	}
+	else if (y1 == y2)
+	{
+		CG_DrawPic(x1 < x2 ? x1 : x2, y1, abs(x1 - x2), 1, cgs.media.whiteShader);
+	}
+	else
+	{
+		len = (x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1);
+		len = sqrt(len);
+		stepx = (x2 - x1) / len;
+		stepy = (y2 - y1) / len;
+		for (i = 0; i < len; i++)
+		{
+			PutPixel(x1, y1);
+			x1 += stepx;
+			y1 += stepy;
+		}
+	}
+
+	trap_R_SetColor(NULL);
+}
 
 /*
 ==============
