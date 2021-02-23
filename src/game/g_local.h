24a65b2d751a80471f5d388b5ff67ead58f0143a
Aciz <arto.karkulahti@gmail.com>
2021-02-23 21:08:27 +0200

    Fix weapons potentially set at WP_NONE when using team command (#585)
    * Fix potentially uninitalized weapons when using team command
    
    * Change function type & remove unnecessary casting, remove unnecessary userinfo update
    
    * return WP_NONE from G_GetDefaultWeaponForClass if team isn't axis/allies

--- src/game/g_local.h
+++ src/game/g_local.h
@@ -1430,6 +1430,7 @@ void StopFollowing(gentity_t *ent);
 void G_TeamDataForString(const char *teamstr, int clientNum, team_t *team, spectatorState_t *sState, int *specClient);
 qboolean SetTeam(gentity_t *ent, const char *s, qboolean force, weapon_t w1, weapon_t w2, qboolean setweapons);
 void G_SetClientWeapons(gentity_t *ent, weapon_t w1, weapon_t w2, qboolean updateclient);
+weapon_t G_GetDefaultWeaponForClass(gentity_t* ent, char *s, bool primary);
 void Cmd_FollowCycle_f(gentity_t *ent, int dir);
 void Cmd_Kill_f(gentity_t *ent);
 void Cmd_SwapPlacesWithBot_f(gentity_t *ent, int botNum);
