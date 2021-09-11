3d0f1ec2933ac4510d2121ac26266998a69cd561
suburb <31364635+suburbski@users.noreply.github.com>
2021-09-11 11:43:59 +0200

    Prepare compiler flag tests on dev server (#703)

--- cmake/CompilerOptions.cmake
+++ cmake/CompilerOptions.cmake
@@ -30,8 +30,7 @@ function(create_compiler_opts target)
 		-Wno-missing-field-initializers
 		$<$<CONFIG:Release>:
 			-flto              # link time optimizations
-			-O3                # max optimization 
-			-ffast-math>       # fast floating point math
+			-O3>               # max optimization 
 		$<$<CONFIG:Debug>:
 			-Og                # supress optimizations
 			-g3                # generate debug info
