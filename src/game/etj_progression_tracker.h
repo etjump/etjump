935883a85d65db814e1264cfb9f6fd599684c4cd
suburb <31364635+suburbski@users.noreply.github.com>
2021-09-16 06:54:41 +0200

    Update year in remaining files

--- src/game/etj_progression_tracker.h
+++ src/game/etj_progression_tracker.h
@@ -1,7 +1,7 @@
 /*
  * MIT License
  * 
- * Copyright (c) 2020 ETJump team <zero@etjump.com>
+ * Copyright (c) 2021 ETJump team <zero@etjump.com>
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -84,6 +84,3 @@ namespace ETJump
 		std::vector<ProgressionTracker> _progressionTrackers;
 	};
 }
-
-
-
