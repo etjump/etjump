f16997738e269489c83c461e740c04028e5f8641
Arto Karkulahti <arto.karkulahti@gmail.com>
2021-12-23 12:36:36 +0200

    uncristify

--- src/cgame/etj_draw_leaves_handler.h
+++ src/cgame/etj_draw_leaves_handler.h
@@ -1,18 +1,18 @@
 /*
  * MIT License
- * 
+ *
  * Copyright (c) 2021 ETJump team <zero@etjump.com>
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -34,33 +34,33 @@
 
 namespace ETJump
 {
-	class DrawLeavesHandler
-	{
-		const char *shaderName{ "__etjump-leaves-shader__" };
-		std::vector<const char *> leavesShaders{
-			"models/mapobjects/trees_sd/winterbranch01",
-			"models/mapobjects/tree_temperate_sd/leaves_temperate1",
-			"models/mapobjects/tree_temperate_sd/leaves_temperate2",
-			"models/mapobjects/tree_temperate_sd/leaves_temperate3",
-			"models/mapobjects/tree_desert_sd/palm_leaf1",
-			"models/mapobjects/plants_sd/bush_desert1",
-			"models/mapobjects/plants_sd/bush_desert2",
-			"models/mapobjects/plants_sd/bush_snow1",
-			"models/mapobjects/plants_sd/catail1",
-			"models/mapobjects/plants_sd/catail2",
-			"models/mapobjects/plants_sd/deadbranch1",
-			"models/mapobjects/plants_sd/deadbranch1_damp",
-			"models/mapobjects/plants_sd/deadbranch2",
-			"models/mapobjects/plants_sd/deadbranch3",
-			"models/mapobjects/plants_sd/grassfoliage1",
-			"models/mapobjects/plants_sd/grass_dry3",
-			"models/mapobjects/plants_sd/grass_green1",
-		};
-
-		void turnOnLeaves();
-		void turnOffLeaves();
-	public:
-		DrawLeavesHandler();
-		~DrawLeavesHandler();
+class DrawLeavesHandler
+{
+	const char *shaderName{ "__etjump-leaves-shader__" };
+	std::vector<const char *> leavesShaders{
+		"models/mapobjects/trees_sd/winterbranch01",
+		"models/mapobjects/tree_temperate_sd/leaves_temperate1",
+		"models/mapobjects/tree_temperate_sd/leaves_temperate2",
+		"models/mapobjects/tree_temperate_sd/leaves_temperate3",
+		"models/mapobjects/tree_desert_sd/palm_leaf1",
+		"models/mapobjects/plants_sd/bush_desert1",
+		"models/mapobjects/plants_sd/bush_desert2",
+		"models/mapobjects/plants_sd/bush_snow1",
+		"models/mapobjects/plants_sd/catail1",
+		"models/mapobjects/plants_sd/catail2",
+		"models/mapobjects/plants_sd/deadbranch1",
+		"models/mapobjects/plants_sd/deadbranch1_damp",
+		"models/mapobjects/plants_sd/deadbranch2",
+		"models/mapobjects/plants_sd/deadbranch3",
+		"models/mapobjects/plants_sd/grassfoliage1",
+		"models/mapobjects/plants_sd/grass_dry3",
+		"models/mapobjects/plants_sd/grass_green1",
 	};
+
+	void turnOnLeaves();
+	void turnOffLeaves();
+public:
+	DrawLeavesHandler();
+	~DrawLeavesHandler();
+};
 }
