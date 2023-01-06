0319756eb830650559a3642d31c40d531cef5e72
suburb <31364635+suburbski@users.noreply.github.com>
2023-01-06 18:17:40 +0100

    Persist last active time instead of activity bool (#913)

--- src/game/etj_inactivity_timer.cpp
+++ src/game/etj_inactivity_timer.cpp
@@ -1,38 +1,39 @@
 /*
-* MIT License
-*
-* Copyright (c) 2023 ETJump team <zero@etjump.com>
-*
-* Permission is hereby granted, free of charge, to any person obtaining a copy
-* of this software and associated documentation files (the "Software"), to deal
-* in the Software without restriction, including without limitation the rights
-* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-* copies of the Software, and to permit persons to whom the Software is
-* furnished to do so, subject to the following conditions:
-*
-* The above copyright notice and this permission notice shall be included in
-* all copies or substantial portions of the Software.
-*
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-* SOFTWARE.
-*/
+ * MIT License
+ *
+ * Copyright (c) 2023 ETJump team <zero@etjump.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
 #include "etj_inactivity_timer.h"
 
 namespace ETJump {
 void InactivityTimer::checkClientInactivity(gentity_t *ent) {
   if (ent->client->pers.cmd.buttons & BUTTON_ANY) {
-    ent->client->realInactivityTime = level.time + 1000 * clientInactivityTimer;
-    ent->client->sess.clientIsInactive = false;
+    ent->client->inactive = false;
+    ent->client->sess.clientLastActive = level.time;
     UpdateClientConfigString(*ent);
-  } else if (level.time > ent->client->realInactivityTime) {
-    ent->client->realInactivityTime = level.time + 1000 * clientInactivityTimer;
-    ent->client->sess.clientIsInactive = true;
+  } else if (!ent->client->inactive &&
+             level.time >= ent->client->sess.clientLastActive +
+                               1000 * clientInactivityTimer) {
+    ent->client->inactive = true;
     UpdateClientConfigString(*ent);
   }
 }
