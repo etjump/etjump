f05a6818cf2a415950db80bc8e918d180aeef8b2
suburb <31364635+suburbski@users.noreply.github.com>
2023-07-20 17:07:07 +0200

    Fix item model out of bounds access (#969)

--- src/game/g_items.cpp
+++ src/game/g_items.cpp
@@ -1208,7 +1208,7 @@ void FinishSpawningItem(gentity_t *ent) {
     // or ammo item specify it as "multi-stage" TTimo left-hand
     // operand of comma expression has no effect initial line:
     // for(i=0;i<4,ent->item->world_model[i];i++) {}
-    for (i = 0; i < 4 && ent->item->world_model[i]; i++) {
+    for (i = 0; i < MAX_ITEM_MODELS && ent->item->world_model[i]; i++) {
     }
 
     ent->s.density = i - 1; // store number of stages in 'density' for client
