808feaaad66f39416ddd96e100328956374d1175
suburb <31364635+suburbski@users.noreply.github.com>
2023-07-20 17:55:22 +0200

    Fix another server filter out of bounds access

--- src/ui/ui_main.cpp
+++ src/ui/ui_main.cpp
@@ -2325,7 +2325,7 @@ static void UI_DrawMissionBriefingObjectives(rectDef_t *rect, float scale,
 static void UI_DrawNetFilter(rectDef_t *rect, float scale, vec4_t color,
                              int textStyle) {
   if (ui_serverFilterType.integer < 0 ||
-      ui_serverFilterType.integer > numServerFilters) {
+      ui_serverFilterType.integer >= numServerFilters) {
     ui_serverFilterType.integer = 0;
   }
   Text_Paint(
