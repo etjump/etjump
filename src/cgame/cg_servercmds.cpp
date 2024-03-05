082f5a92452d53f961bd65bcba7f428c36511308
Aciz <arto.karkulahti@gmail.com>
2024-03-05 22:15:30 +0200

    Fix dlights not updating on runtime when spawned via mapscript (#1245)
    dlight configstring updates were not handled on client if a dlight was spawned via mapscript after initial map load

--- src/cgame/cg_servercmds.cpp
+++ src/cgame/cg_servercmds.cpp
@@ -741,7 +741,7 @@ static void CG_ConfigStringModified(void) {
   } else if (num >= CS_PLAYERS && num < CS_PLAYERS + MAX_CLIENTS) {
     CG_NewClientInfo(num - CS_PLAYERS);
   } else if (num >= CS_DLIGHTS && num < CS_DLIGHTS + MAX_DLIGHT_CONFIGSTRINGS) {
-    // FIXME - dlight changes ignored!
+    CG_SetupDlightstyles();
   } else if (num == CS_SHADERSTATE) {
     CG_ShaderStateChanged();
   } else if (num == CS_CHARGETIMES) {
