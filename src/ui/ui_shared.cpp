e641ac64e859aa78f06302e08ec1c16498024456
Aciz <arto.karkulahti@gmail.com>
2024-09-26 03:08:20 +0300

    Increase UI memory pool
    Recent UI work has increased UI memory usage quite a bit, and x86 wasn't actually even running anymore, was ~20% over memory limits. Also add 'ui_report' to autocomplete.

--- src/ui/ui_shared.cpp
+++ src/ui/ui_shared.cpp
@@ -77,9 +77,9 @@ static qboolean Menu_OverActiveItem(menuDef_t *menu, float x, float y);
   #endif
 #else
   #if id386
-    #define MEM_POOL_SIZE (4096 * 1024) // Arnout: was 1024
+    #define MEM_POOL_SIZE (8192 * 1024) // Arnout: was 1024
   #else
-    #define MEM_POOL_SIZE (8192 * 1024)
+    #define MEM_POOL_SIZE (16384 * 1024)
   #endif
 #endif
 
