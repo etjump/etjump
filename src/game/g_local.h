86531d4999896e05f51f364be87f54134b942677
Aciz <arto.karkulahti@gmail.com>
2024-09-28 19:40:00 +0300

    Fix shooter entities potentially crashing the server (#1467)
    Don't fire the entities if we're approaching entity limits.

--- src/game/g_local.h
+++ src/game/g_local.h
@@ -1549,7 +1549,6 @@ gentity_t *G_PopupMessage(popupMessageType_t type);
 void G_Sound(gentity_t *ent, int soundIndex);
 void G_AnimScriptSound(int soundIndex, vec3_t org, int client);
 void G_FreeEntity(gentity_t *e);
-// qboolean	G_EntitiesFree( void );
 
 void G_TouchTriggers(gentity_t *ent);
 
