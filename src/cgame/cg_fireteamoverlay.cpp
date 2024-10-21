544ac1a4297c176f8993fb8a09d2d02e8ce54466
Aciz <arto.karkulahti@gmail.com>
2024-10-22 02:11:24 +0300

    Disallow etj_hideMe if fireteam collision is enabled
    Because hidden players aren't added as solid entities on client, having hideme enabled while fireteam collision is enabled breaks collision and freezes the hidden players playermodel in place.

--- src/cgame/cg_fireteamoverlay.cpp
+++ src/cgame/cg_fireteamoverlay.cpp
@@ -129,6 +129,11 @@ void CG_ParseFireteams() {
       if (COM_BitCheck(clnts, j)) {
         cg.fireTeams[i].joinOrder[j] = qtrue;
         cgs.clientinfo[j].fireteamData = &cg.fireTeams[i];
+
+        if (CG_IsOnFireteam(cg.clientNum) &&
+            cgs.clientinfo[i].fireteamData->noGhost && etj_hideMe.integer) {
+          trap_Cvar_Set("etj_hideMe", "0");
+        }
       } else {
         cg.fireTeams[i].joinOrder[j] = qfalse;
       }
