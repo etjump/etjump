f2982dc977e9e29d730f657de456d6fc3587125b
Aciz <arto.karkulahti@gmail.com>
2025-03-08 19:19:38 +0200

    Cap dlight stylestring length on spawn
    Since the cgame array is 64 characters, there's no need to ever write more than 64 characters as the length to configstrings either.

--- src/cgame/cg_local.h
+++ src/cgame/cg_local.h
@@ -330,7 +330,7 @@ typedef struct centity_s {
   int dl_oldframe;
   float dl_backlerp;
   int dl_time;
-  char dl_stylestring[64];
+  char dl_stylestring[MAX_DLIGHT_STYLESTRING];
   int dl_sound;
   int dl_atten;
 
