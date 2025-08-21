7327ebc4216e05c852a29aa7cb9fe70c7cf3397a
Aciz <arto.karkulahti@gmail.com>
2025-08-21 17:40:33 +0300

    Add cvar to hide flamethrower effects
    'etj_hideFlamethrowerEffects' allows hiding flamethrower effects (flame sprites, flame stream, dlights).
    
    Bitflag cvar:
    * 1 = hide effects produced by the player
    * 2 = hide effects produced by others

--- src/cgame/cg_main.cpp
+++ src/cgame/cg_main.cpp
@@ -689,6 +689,8 @@ vmCvar_t etj_HUD_noLerp;
 
 vmCvar_t etj_useExecQuiet;
 
+vmCvar_t etj_hideFlamethrowerEffects;
+
 typedef struct {
   vmCvar_t *vmCvar;
   const char *cvarName;
@@ -1296,6 +1298,9 @@ cvarTable_t cvarTable[] = {
 
     {&etj_HUD_noLerp, "etj_HUD_noLerp", "0", CVAR_ARCHIVE},
     {&etj_useExecQuiet, "etj_useExecQuiet", "0", CVAR_ARCHIVE},
+
+    {&etj_hideFlamethrowerEffects, "etj_hideFlamethrowerEffects", "0",
+     CVAR_ARCHIVE},
 };
 
 int cvarTableSize = sizeof(cvarTable) / sizeof(cvarTable[0]);
