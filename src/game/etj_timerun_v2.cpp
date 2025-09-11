etjump/etjump

commit 329158a4c684addd5d84d2bf6d11b513a254ba9a

Fix target_init + timeruns not resetting player ammo correctly (#1515)
When players weapons were removed by target_init or timerun start, the ammo for the weapons wasn't cleared. This caused an issue with weapons that do no use clips, where picking up the weapon after it was removed would restore the ammo count the player had when the weapon was taken away, instead of having whatever ammo the picked up weapon had.

src/game/etj_timerun_v2.cpp
@@ -1550,3 +1550,50 @@ const ETJump::Timerun::Season *ETJump::TimerunV2::getMostRelevantSeason() {
   }
   return mostRelevant;
 }
+
+void ETJump::TimerunV2::removeDisallowedWeapons(gentity_t *ent) {
+  for (int i = 0; i < WP_NUM_WEAPONS; i++) {
+    if (BG_WeaponDisallowedInTimeruns(i)) {
+      COM_BitClear(ent->client->ps.weapons, i);
+      ent->client->ps.ammo[i] = 0;
+      ent->client->ps.ammoclip[i] = 0;
+    }
+  }
+
+  // expire grenades
+  ent->client->ps.grenadeTimeLeft = 0;
+}
+
+void ETJump::TimerunV2::removePlayerProjectiles(gentity_t *ent) {
+  for (int i = MAX_CLIENTS + BODY_QUEUE_SIZE; i < level.num_entities; i++) {
+    if (ent->s.eType == ET_MISSILE) {
+      if (ent->parent && ent->parent->s.number == ClientNum(ent)) {
+        G_FreeEntity(ent);
+      }
+    }
+  }
+}
+
+bool ETJump::TimerunV2::weaponIsExplosivePickup(const int weapon) {
+  // FIXME: we should allow K43/Garand, but remove rifle nades on pickup
+  switch (weapon) {
+    case WP_GRENADE_LAUNCHER:
+    case WP_PANZERFAUST:
+    case WP_FLAMETHROWER:
+    case WP_GRENADE_PINEAPPLE:
+    case WP_DYNAMITE:
+    case WP_PLIERS:
+    case WP_KAR98:
+    case WP_CARBINE:
+    case WP_LANDMINE:
+    case WP_SATCHEL:
+    case WP_SATCHEL_DET:
+    case WP_MORTAR:
+    case WP_GPG40:
+    case WP_M7:
+    case WP_MORTAR_SET:
+      return true;
+    default:
+      return false;
+  }
+}

src/game/bg_misc.cpp
@@ -2795,28 +2795,7 @@ qboolean BG_WeaponInWolfMP(int weapon) {
   }
 }
 
-qboolean BG_WeaponIsExplosive(int weap) {
-  switch (weap) {
-    case WP_CARBINE:
-    case WP_KAR98:
-    case WP_M7:
-    case WP_GRENADE_LAUNCHER:
-    case WP_GRENADE_PINEAPPLE:
-    case WP_PANZERFAUST:
-    case WP_LANDMINE:
-    case WP_ARTY:
-    case WP_GPG40:
-    case WP_FLAMETHROWER:
-    case WP_MORTAR:
-    case WP_SATCHEL:
-    case WP_DYNAMITE:
-      return qtrue;
-    default:
-      return qfalse;
-  }
-}
-
-bool BG_WeaponDisallowedInTimeruns(int weap) {
+bool BG_WeaponDisallowedInTimeruns(const int weap) {
   switch (weap) {
     case WP_DYNAMITE:
     case WP_GRENADE_LAUNCHER:

src/game/etj_target_init.cpp
@@ -27,14 +27,15 @@
 
 #include "g_local.h"
 #include "etj_target_init.h"
+#include "etj_utilities.h"
 
 namespace ETJump {
-std::vector<weapon_t> allowedWeapons = {
+static constexpr std::array<weapon_t, 7> allowedWeapons = {
     WP_NONE,   WP_KNIFE,      WP_MEDKIT, WP_MEDIC_SYRINGE, WP_MEDIC_ADRENALINE,
     WP_PLIERS, WP_BINOCULARS,
 };
 
-void TargetInit::spawn(gentity_t *self) { self->use = TargetInit::use; }
+void TargetInit::spawn(gentity_t *self) { self->use = use; }
 
 void TargetInit::use(gentity_t *self, gentity_t *other, gentity_t *activator) {
   if (!activator || !activator->client) {
@@ -65,7 +66,7 @@ void TargetInit::use(gentity_t *self, gentity_t *other, gentity_t *activator) {
     }
 
     // restore the ammo we spawned with in case we don't have our weapon anymore
-    if (!(COM_BitCheck(activator->client->ps.weapons, weapon))) {
+    if (!COM_BitCheck(activator->client->ps.weapons, weapon)) {
       activator->client->ps.ammo[weapon] =
           activator->client->sess.ammoOnSpawn[weapon];
       activator->client->ps.ammoclip[weapon] =
@@ -89,26 +90,18 @@ void TargetInit::use(gentity_t *self, gentity_t *other, gentity_t *activator) {
         }
       }
     }
-    // swap to the restored weapon if our current weapon was removed
-    if (!(COM_BitCheck(activator->client->ps.weapons,
-                       activator->client->ps.weapon))) {
-      activator->client->ps.weapon = weapon;
+
+    // swap to a valid weapon if our current weapon was removed
+    if (!COM_BitCheck(activator->client->ps.weapons,
+                      activator->client->ps.weapon)) {
+      Utilities::selectValidWeapon(activator);
     }
   }
 
   if (!(self->spawnflags & static_cast<int>(SpawnFlags::KeepPortalgun))) {
     // swap weapons in case portalgun was equipped
-    // if we have ammo: primary > secondary > knife
     if (activator->client->ps.weapon == WP_PORTAL_GUN) {
-      if (BG_WeaponHasAmmo(&activator->client->ps,
-                           activator->client->sess.playerWeapon)) {
-        activator->client->ps.weapon = activator->client->sess.playerWeapon;
-      } else if (BG_WeaponHasAmmo(&activator->client->ps,
-                                  activator->client->sess.playerWeapon2)) {
-        activator->client->ps.weapon = activator->client->sess.playerWeapon2;
-      } else {
-        activator->client->ps.weapon = WP_KNIFE;
-      }
+      Utilities::selectValidWeapon(activator);
     }
 
     COM_BitClear(activator->client->ps.weapons, WP_PORTAL_GUN);
@@ -131,9 +124,9 @@ void TargetInit::use(gentity_t *self, gentity_t *other, gentity_t *activator) {
 
   if (self->spawnflags & static_cast<int>(SpawnFlags::RemoveStartingWeapons) &&
       !(self->spawnflags & static_cast<int>(SpawnFlags::KeepWeapons))) {
-    for (int i = 0; i < WP_NUM_WEAPONS; i++) {
-      if (std::find(allowedWeapons.begin(), allowedWeapons.end(), i) !=
-          allowedWeapons.end()) {
+    for (int i = WP_NONE; i < WP_NUM_WEAPONS; i++) {
+      if (std::find(allowedWeapons.cbegin(), allowedWeapons.cend(), i) !=
+          allowedWeapons.cend()) {
         continue;
       }
 

src/game/etj_timerun_v2.h
@@ -115,6 +115,13 @@ public:
   void printSeasons(int clientNum);
   void deleteSeason(int clientNum, const std::string &name);
 
+  static void removeDisallowedWeapons(gentity_t *ent);
+  static void removePlayerProjectiles(gentity_t *ent);
+
+  // returns true if weapon is considered an explosive by the
+  // "No explosives" timerun spawnflag
+  static bool weaponIsExplosivePickup(int weapon);
+
 private:
   void startNotify(Player *player) const;
   static bool isDebugging(int clientNum);

src/game/etj_utilities.cpp
@@ -27,6 +27,7 @@
 #include "etj_utilities.h"
 #include "etj_save_system.h"
 #include "etj_map_statistics.h"
+#include "etj_timerun_v2.h"
 
 std::vector<int> Utilities::getSpectators(int clientNum) {
   std::vector<int> spectators;
@@ -54,42 +55,6 @@ std::vector<int> Utilities::getSpectators(int clientNum) {
   return spectators;
 }
 
-static void SelectCorrectWeapon(gclient_t *cl) {
-  auto primary = cl->sess.playerWeapon;
-  auto secondary = cl->sess.playerWeapon2;
-
-  // early out if our currently held weapon is allowed
-  // even though disallowed weapons are already removed, we'll still get to
-  // keep any if it's currently equipped, so we force a weapon swap later
-  if (!BG_WeaponDisallowedInTimeruns(cl->ps.weapon)) {
-    return;
-  }
-
-  // our currently held weapon was removed, so swap to a valid one
-  if (COM_BitCheck(cl->ps.weapons, primary) &&
-      BG_WeaponHasAmmo(&cl->ps, primary)) {
-    cl->ps.weapon = primary;
-  } else if (COM_BitCheck(cl->ps.weapons, secondary) &&
-             BG_WeaponHasAmmo(&cl->ps, secondary)) {
-    cl->ps.weapon = secondary;
-  } else {
-    cl->ps.weapon = WP_KNIFE;
-  }
-}
-
-static void RemovePlayerProjectiles(int clientNum) {
-  // Iterate entitylist and remove all projectiles
-  // that belong to the activator
-  for (int i = MAX_CLIENTS; i < level.num_entities; i++) {
-    auto ent = g_entities + i;
-    if (ent->s.eType == ET_MISSILE) {
-      if (ent->parent && ent->parent->s.number == clientNum) {
-        G_FreeEntity(ent);
-      }
-    }
-  }
-}
-
 void Utilities::startRun(int clientNum) {
   gentity_t *player = g_entities + clientNum;
 
@@ -106,9 +71,14 @@ void Utilities::startRun(int clientNum) {
     return;
   }
 
-  RemovePlayerWeapons(clientNum);
-  RemovePlayerProjectiles(clientNum);
-  SelectCorrectWeapon(player->client);
+  ETJump::TimerunV2::removeDisallowedWeapons(player);
+  ETJump::TimerunV2::removePlayerProjectiles(player);
+
+  // force swap if the current weapon isn't allowed in timeruns
+  if (BG_WeaponDisallowedInTimeruns(player->client->ps.weapon)) {
+    selectValidWeapon(player);
+  }
+
   ClearPortals(player);
 }
 
@@ -119,16 +89,16 @@ void Utilities::stopRun(int clientNum) {
   ETJump::UpdateClientConfigString(*player);
 }
 
-void Utilities::RemovePlayerWeapons(int clientNum) {
-  auto *cl = (g_entities + clientNum)->client;
-
-  for (int i = 0; i < WP_NUM_WEAPONS; i++) {
-    if (BG_WeaponDisallowedInTimeruns(i)) {
-      COM_BitClear(cl->ps.weapons, i);
-    }
+void Utilities::selectValidWeapon(const gentity_t *ent) {
+  // primary > secondary > knife
+  if (BG_WeaponHasAmmo(&ent->client->ps, ent->client->sess.playerWeapon)) {
+    ent->client->ps.weapon = ent->client->sess.playerWeapon;
+  } else if (BG_WeaponHasAmmo(&ent->client->ps,
+                              ent->client->sess.playerWeapon2)) {
+    ent->client->ps.weapon = ent->client->sess.playerWeapon2;
+  } else {
+    ent->client->ps.weapon = WP_KNIFE;
   }
-
-  cl->ps.grenadeTimeLeft = 0;
 }
 
 bool Utilities::inNoNoclipArea(gentity_t *ent) {

src/game/etj_utilities.h
@@ -24,9 +24,8 @@
 
 #pragma once
 
-#include <algorithm>
-#include <iterator>
 #include <vector>
+
 #include "etj_levels.h"
 
 namespace Utilities {
@@ -81,7 +80,10 @@ std::string timestampToString(int timestamp,
                               const char *format = "%d/%m/%y %H:%M:%S",
                               const char *start = "never");
 
-void RemovePlayerWeapons(int clientNum);
+/**
+ * Sets a valid weapon as currently held weapon
+ */
+void selectValidWeapon(const gentity_t *ent);
 
 /**
  * Returns true if the player is inside a no-noclip area

src/game/g_client.cpp
@@ -741,12 +741,16 @@ Resets players ammo to default
 =================================
 */
 void ResetPlayerAmmo(gclient_t *client, gentity_t *ent) {
-  auto ammoMultiplier = 0;
-  auto nadeCount = 0;
+  // clear everything - AddWeaponToPlayer adds back the correct amount of ammo
+  memset(client->ps.ammo, 0, MAX_WEAPONS * sizeof(int));
+  memset(client->ps.ammoclip, 0, MAX_WEAPONS * sizeof(int));
 
-  // set default ammo multiplier for thompson/mp40/sten
+  // set default ammo multiplier for thompson/mp40/sten,
   // and grenade count per class, as these do not follow ammotable data
   // ammo from skill bonuses is handled in AddWeaponToPlayer
+  int ammoMultiplier = 0;
+  int nadeCount = 0;
+
   switch (client->sess.playerType) {
     case PC_SOLDIER:
       ammoMultiplier = 2;
@@ -777,26 +781,31 @@ void ResetPlayerAmmo(gclient_t *client, gentity_t *ent) {
   // grenades are cleared from ps.weapons when they run out of ammo so
   // they are handled seperately dynamite, pliers, satchel & satchel
   // detonator have 0 ammo in ammotable so they must be given manually
-  for (auto i = 0; i < WP_NUM_WEAPONS; i++) {
+  for (int i = WP_NONE; i < WP_NUM_WEAPONS; i++) {
     if (COM_BitCheck(client->ps.weapons, i)) {
       if (i == WP_GRENADE_LAUNCHER || i == WP_GRENADE_PINEAPPLE) {
         continue;
       }
+
       if (client->sess.timerunActive && BG_WeaponDisallowedInTimeruns(i)) {
         continue;
       }
+
       if ((i == WP_DYNAMITE || i == WP_PLIERS) && level.noExplosives != 2) {
         AddWeaponToPlayer(client, static_cast<weapon_t>(i), 0, 1, qfalse);
         continue;
       }
+
       if (i == WP_SATCHEL && !level.noExplosives) {
         AddWeaponToPlayer(client, WP_SATCHEL, 0, 1, qfalse);
         AddWeaponToPlayer(client, WP_SATCHEL_DET, 0, 0, qfalse);
         continue;
       }
-      if (BG_WeaponIsExplosive(i) && level.noExplosives) {
+
+      if (level.noExplosives && !ETJump::weaponAllowedWithNoExplosives(i)) {
         continue;
       }
+
       if (i == WP_MP40 || i == WP_THOMPSON || i == WP_STEN) {
         AddWeaponToPlayer(client, static_cast<weapon_t>(i),
                           GetAmmoTableData(i)->defaultStartingAmmo *

src/game/g_items.cpp
@@ -10,6 +10,7 @@
  */
 
 #include "g_local.h"
+#include "etj_timerun_v2.h"
 
 #define RESPAWN_SP -1
 #define RESPAWN_KEY 4
@@ -859,10 +860,10 @@ void Touch_Item(gentity_t *ent, gentity_t *other, trace_t *trace) {
   }
 
   // ETJump: disable explosives pickup
-  if (BG_WeaponIsExplosive(ent->item->giTag) &&
+  if (ETJump::TimerunV2::weaponIsExplosivePickup(ent->item->giTag) &&
       other->client->sess.timerunActive &&
-      (other->client->sess.runSpawnflags &
-       static_cast<int>(ETJump::TimerunSpawnflags::NoExplosivesPickup))) {
+      other->client->sess.runSpawnflags &
+          static_cast<int>(ETJump::TimerunSpawnflags::NoExplosivesPickup)) {
     return;
   }
 

commit 0be7550e35561336ed1106c91828e125d7862aa8

Fix projectiles not getting deleted on timerun start
This was accidentally broken in 329158a4c684addd5d84d2bf6d11b513a254ba9a.

src/game/etj_timerun_v2.cpp
@@ -1613,9 +1613,11 @@ void ETJump::TimerunV2::removeDisallowedWeapons(gentity_t *ent) {
 
 void ETJump::TimerunV2::removePlayerProjectiles(gentity_t *ent) {
   for (int i = MAX_CLIENTS + BODY_QUEUE_SIZE; i < level.num_entities; i++) {
-    if (ent->s.eType == ET_MISSILE) {
-      if (ent->parent && ent->parent->s.number == ClientNum(ent)) {
-        G_FreeEntity(ent);
+    gentity_t *e = &g_entities[i];
+
+    if (e->s.eType == ET_MISSILE) {
+      if (e->parent && e->parent->s.number == ClientNum(ent)) {
+        G_FreeEntity(e);
       }
     }
   }