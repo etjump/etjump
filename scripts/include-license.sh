f2228f6157be86094b9fce868d9d82cecc5a3caf
Aciz <arto.karkulahti@gmail.com>
2025-11-08 14:57:26 +0200

    Format shell scripts with shfmt
    Also moved the release script to 'scripts' directory, duh

--- scripts/include-license.sh
+++ scripts/include-license.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-for i in $(find src -name 'etj_*.cpp' -o -name 'etj_*.h' -o -name 'etj_*.hpp');
-do
-  if ! grep -q Copyright "$i"
-  then
-  	echo "/*" >> "$i.new"
-    cat LICENSE | while read line; do echo " * $line"; done >> "$i.new"
-    echo $' */\n' >> "$i.new"
-    cat "$i" >> "$i.new" 
+for i in $(find src -name 'etj_*.cpp' -o -name 'etj_*.h' -o -name 'etj_*.hpp'); do
+  if ! grep -q Copyright "$i"; then
+    echo "/*" >>"$i.new"
+    cat LICENSE | while read line; do echo " * $line"; done >>"$i.new"
+    echo $' */\n' >>"$i.new"
+    cat "$i" >>"$i.new"
     mv "$i.new" "$i"
   fi
 done
