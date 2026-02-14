6fa3a5cd0cffe8db31f10e12fa7170fcfa903a4b
Aciz <arto.karkulahti@gmail.com>
2026-02-14 21:00:19 +0200

    Workaround broken command tokenizer in ETe
    When using rcon in localhost on ETe, the engines command tokenizer performs illegal 'strncpy' operations with overlapping 'src' & 'dest' memory regions, which sometimes results in the server getting an empty client command, potentially crashing the server.

--- src/game/g_cmds.cpp
+++ src/game/g_cmds.cpp
@@ -4995,6 +4995,12 @@ void ClientCommand(int clientNum) {
   }
   trap_Argv(0, cmd, sizeof(cmd));
 
+  // workaround for ETe's broken command tokenizer,
+  // which sometimes sends empty client commands on localhost
+  if (cmd[0] == '\0') {
+    return;
+  }
+
   if (OnClientCommand(ent)) {
     return;
   }
