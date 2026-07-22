5c3461f4d3b00fcd59c73f09b6b932786cf3c2e3
Aciz <arto.karkulahti@gmail.com>
2026-07-22 12:54:37 +0300

    Add cached cvar support for edit/numericfields
    When edit/numericfield is marked with 'cacheCvar', the cvar value only
    updates once the editing is finished.

--- src/ui/ui_shared.h
+++ src/ui/ui_shared.h
@@ -371,7 +371,7 @@ typedef struct itemDef_s {
 
   textScroll_t textScroll;
 
-  // update cvar value only when itemCapture ends
+  // sliders & editfields - update cvar value only when itemCapture/edit ends
   bool cacheCvar;
   // allocated if 'cacheCvar' is used on an item, 'MAX_CVAR_VALUE_STRING' bytes
   char *cacheCvarValue;
