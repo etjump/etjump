b6bb1acd1d8c796ff8bcc3c2c5c2f27d7a2a17bf
Aciz <arto.karkulahti@gmail.com>
2026-09-15 01:20:41 +0300

    Remove NormalizeColor
    Unused

--- src/game/q_shared.h
+++ src/game/q_shared.h
@@ -646,8 +646,6 @@ void _VectorMA(const vec3_t veca, float scale, const vec3_t vecb, vec3_t vecc);
 unsigned ColorBytes3(float r, float g, float b);
 unsigned ColorBytes4(float r, float g, float b, float a);
 
-float NormalizeColor(const vec3_t in, vec3_t out);
-
 float RadiusFromBounds(const vec3_t mins, const vec3_t maxs);
 void ClearBounds(vec3_t mins, vec3_t maxs);
 void AddPointToBounds(const vec3_t v, vec3_t mins, vec3_t maxs);
