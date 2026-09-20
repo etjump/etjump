26d1a5e4ebf6ff94f350171c4344b570f4b1bfd7
Aciz <arto.karkulahti@gmail.com>
2026-09-21 00:06:11 +0300

    Fix PmoveUtils prone check when cheats are enabled
    Need braces around the trace check due to operator precedence.

--- src/cgame/etj_pmove_utils_v2.cpp
+++ src/cgame/etj_pmove_utils_v2.cpp
@@ -476,8 +476,9 @@ bool PmoveUtilsV2::checkProne(pmove_t &pm) {
   pm.trace(&trace, pm.ps->origin, pm.ps->mins, pm.ps->maxs, pm.ps->origin,
            pm.ps->clientNum, CONTENTS_NOPRONE);
 
-  if (!cgs.cheats && (pm.shared & BG_LEVEL_NO_PRONE) ? trace.fraction == 1.0f
-                                                     : trace.fraction != 1.0f) {
+  if (!cgs.cheats &&
+      ((pm.shared & BG_LEVEL_NO_PRONE) ? trace.fraction == 1.0f
+                                       : trace.fraction != 1.0f)) {
     pm.ps->eFlags &= ~EF_PRONE;
     pm.ps->eFlags &= ~EF_PRONE_MOVING;
     return false;
